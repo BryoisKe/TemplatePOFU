ae442b4290fea71b760ac9284d3c866e1da9af8f
Kenny <kenny-bryois@hotmail.fr>
2017-11-28 13:43:50 +0100

    Add file for captcha

--- add_temoignage.php
+++ add_temoignage.php
@@ -1,5 +1,35 @@
 <?php
 session_start();
+try {
+    include_once './inc/connexion.inc.php';
+    $bdd = getConnexion('pofu');
+} catch (Exception $ex) {
+    
+}
+//if(isset($_POST['temoignage'])){
+//    if(!empty($_POST['temoignage'])){
+//        echo 'oui';
+//    }
+//}
+
+ require('recaptcha/autoload.php');
+  if(isset($_POST['temoignage'])) {
+    if(isset($_POST['g-recaptcha-response'])) {
+      $recaptcha = new \ReCaptcha\ReCaptcha('6Lc2ljoUAAAAAIs7MZjKw_fr_RPxM4cnbMQVRCrL');
+      $resp = $recaptcha->verify($_POST['g-recaptcha-response']);
+      if ($resp->isSuccess()) {
+          var_dump('Captcha Valide');
+      } else {
+          $errors = $resp->getErrorCodes();
+          var_dump('Captcha Invalide');
+          var_dump($errors);
+      }
+    } else {
+      var_dump('Captcha non rempli');
+    }
+  }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +45,7 @@ session_start();
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
-
+       <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
     <body>
 
@@ -37,35 +67,34 @@ session_start();
                         Témoignage
                     </h1>
                     <div class="row">
-                        <div class="col-md-12">
-                            <center>
-                                <form>
-                                    <textarea placeholder="Votre témoignage..." rows="10" cols="100"></textarea>
-                                </form>
-                            </center>
-                        </div>
+                        <center>
+                            <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
+                                <div class="col-md-12">
+                                    <textarea placeholder="Votre témoignage..." rows="10" cols="100" name="temoignage"></textarea>
+                                </div>
+                                <br>
+                                <div class="g-recaptcha col-md-12" data-sitekey="6Lc2ljoUAAAAAIs7MZjKw_fr_RPxM4cnbMQVRCrL"></div>
+                                <div class="col-md-12">
+                                    <a href="<?php$_SERVER['PHP_SELF']?>"><button type="submit" class="btn btn-primary">Poster</button></a> 
+                                    <a href="temoignage.php"><button type="button" class="btn btn-primary">Annuler</button></a>
+                                </div>
+                            </form>
+                        </center>
                     </div>
-
-                    <br>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="temoignage.html"><button type="button" class="btn btn-primary">Envoyer</button></a> 
-                            <a href="temoignage.html"><button type="button" class="btn btn-primary">Annuler</button></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
                 </div>
             </div>
-            <br>
-            <div class="row footer">
-                <div class="col-md-12">
-                    Test  
-                </div>
+            <div class="col-md-2">
             </div>
         </div>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
+        <br>
+        <div class="row footer">
+            <div class="col-md-12">
+                Test  
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
 </html>
