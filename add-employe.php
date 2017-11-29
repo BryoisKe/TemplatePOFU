387bb72b5eb1d6c482c9cfd66184b32ddba3d4e3
Kenny <kenny-bryois@hotmail.fr>
2017-11-29 11:28:06 +0100

    Create a exemple for upload image

--- add-employe.php
+++ add-employe.php
@@ -41,7 +41,7 @@ if (isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['telephone
         $rqtAddEmploye->bindValue(':nom', $nom);
         $rqtAddEmploye->bindValue(':email', $email);
         $rqtAddEmploye->bindValue(':telephone', $telephone);
-        $rqtAddEmploye->bindValue(':image', $nom_image);
+        $rqtAddEmploye->bindValue(':image', 'no-image.png');
         $rqtAddEmploye->bindValue(':genre', $genre);
         $rqtAddEmploye->bindValue(':role', $role);
 
@@ -50,6 +50,34 @@ if (isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['telephone
         exit();
     } else {
         $msgErreur = 'Veuillez remplir un champ';
+    }
+}
+if (isset($_FILES['avatar'])) {
+    $dossier = './img/employe/';
+    $fichier = basename($_FILES['avatar']['name']);
+    $taille_maxi = 100000;
+    $taille = filesize($_FILES['avatar']['tmp_name']);
+    $extensions = array('.png', '.jpg', '.jpeg');
+    $extension = strrchr($_FILES['avatar']['name'], '.');
+//Début des vérifications de sécurité...
+    if (!in_array($extension, $extensions)) { //Si l'extension n'est pas dans le tableau
+        $erreur = 'Vous devez uploader un fichier de type png, jpg, jpeg';
+    }
+    if ($taille > $taille_maxi) {
+        $erreur = 'Le fichier est trop gros...';
+    }
+    if (!isset($erreur)) { //S'il n'y a pas d'erreur, on upload
+        //On formate le nom du fichier ici...
+        $fichier = strtr($fichier, 'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+        $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+        if (move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier)) { //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+            echo 'Upload effectué avec succès !';
+            header("Location: contact.php");
+        } else { //Sinon (la fonction renvoie FALSE).
+            echo 'Echec de l\'upload !';
+        }
+    } else {
+        echo $erreur;
     }
 }
 ?>
@@ -90,53 +118,58 @@ if (isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['telephone
                     </h1>
                     <div class="row">
 
-                            <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
-                                <div class="row form-addemp">
-                                    <div class="form-group col-sm-6">
-                                        <label>Genre</label>
-                                        <select name="genre" class="form-control">
-                                            <option value="homme">Monsieur</option>
-                                            <option value="femme">Madame</option>
-                                        </select>
-                                    </div>
+                        <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data">
+                            <div class="row form-addemp">
+                                <div class="form-group col-sm-6">
+                                    <label>Genre</label>
+                                    <select name="genre" class="form-control">
+                                        <option value="homme">Monsieur</option>
+                                        <option value="femme">Madame</option>
+                                    </select>
                                 </div>
-                                <div class="row form-addemp">
-                                    <div class="form-group col-sm-6">
-                                        <label>Nom :</label>
-                                        <input type="text" name="nom" class="form-control" id="" value="<?php echo $nom; ?>">
-                                    </div>
-
-                                    <div class="form-group col-sm-6">
-                                        <label>Prénom :</label>
-                                        <input type="text" name="prenom" class="form-control" id="" value="<?php echo $prenom; ?>">
-                                    </div>
-
-                                    <div class="form-group col-sm-6">
-                                        <label>Numéro de téléphone :</label>
-                                        <input type="text" name="telephone" class="form-control" id="" value="<?php echo $telephone; ?>">
-                                    </div>
-
-                                    <div class="form-group col-sm-6">
-                                        <label>Nom de l'image : ( avec extension )</label>
-                                        <input type="" name="image" class="form-control" id="" value="<?php echo $nom_image; ?>">
-                                    </div>
-                                    <div class="form-group col-sm-12">
-                                        <label>Adresse mail :</label>
-                                        <input type="email" name="email" class="form-control" id="" value="<?php echo $email; ?>">
-                                    </div>
-
-                                    <div class="col-sm-6">
-                                        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Valider"/>
-                                    </div>
-
-                                    <div class="col-sm-6">
-                                        <a href="contact.php" class="btn btn-lg btn-primary btn-block">Annuler</a>
-                                    </div>
-                                </div>
-                            </form>
-                            <div>
-                                <?php echo $msgErreur; ?>
                             </div>
+                            <div class="row form-addemp">
+                                <div class="form-group col-sm-6">
+                                    <label>Nom :</label>
+                                    <input type="text" name="nom" class="form-control" id="" value="<?php echo $nom; ?>">
+                                </div>
+
+                                <div class="form-group col-sm-6">
+                                    <label>Prénom :</label>
+                                    <input type="text" name="prenom" class="form-control" id="" value="<?php echo $prenom; ?>">
+                                </div>
+
+                                <div class="form-group col-sm-6">
+                                    <label>Numéro de téléphone :</label>
+                                    <input type="text" name="telephone" class="form-control" id="" value="<?php echo $telephone; ?>">
+                                </div>
+
+                                <div class="form-group col-sm-6">
+                                    <label>Nom de l'image : ( avec extension )</label>
+                                    <input type="text" name="image" class="form-control" id="imageUpload" value="">
+                                    <input type="hidden" name="MAX_FILE_SIZE" value="100000">
+                                    <input type="file" name="avatar">
+                                </div>
+
+                                <div class="form-group col-sm-12">
+                                    <label>Adresse mail :</label>
+                                    <input type="email" name="email" class="form-control" id="" value="<?php echo $email; ?>">
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <input class="btn btn-lg btn-primary btn-block" type="submit" value="Valider"/>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <a href="contact.php" class="btn btn-lg btn-primary btn-block">Annuler</a>
+                                </div>
+                            </div>
+                        </form>
+                        
+                        <br><br>
+                        <div>
+                            <?php echo $msgErreur; ?>
+                        </div>
                     </div>
                     <div class="col-md-2">
                     </div>
---
+++ inc/upload.inc.php
@@ -0,0 +1,30 @@
+<?php
+$target_dir = "./img/employe";
+$target_file = $target_dir . basename($_FILES["imageUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "Sorry, only JPG, JPEG, PNG files are allowed.";
+    $uploadOk = 0;
+}
+?>
